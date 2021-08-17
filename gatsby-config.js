0a979f7db48a8835a3762ec6da396bc2f4eecf4c
Muhammad Faseeh Ud Din <mfaseehuddin2001@gmail.com>
2021-08-17 12:03:40 +0500

    Queries Figured out

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,43 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "wordpress-test",
-  },
-  plugins: [
-    {
-      resolve: "gatsby-source-wordpress",
-      options: {
-        url: "http://cms.promirage.tech/graphql",
-      },
+    siteMetadata: {
+        siteUrl: "https://www.yourdomain.tld",
+        title: "wordpress-test",
     },
-    "gatsby-plugin-sass",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-  ],
+    plugins: [
+        {
+            resolve: "gatsby-source-wordpress",
+            options: {
+                url: "http://cms.promirage.tech/graphql",
+            },
+            schema: {
+                typePrefix: `Wp`,
+            },
+            develop: {
+                hardCacheMediaFiles: true,
+            },
+            type: {
+                Post: {
+                    limit:
+                        process.env.NODE_ENV === `development`
+                            ? // Lets just pull 50 posts in development to make it easy on ourselves (aka. faster).
+                              50
+                            : // and we don't actually need more than 5000 in production for this particular site
+                              5000,
+                },
+            },
+        },
+        "gatsby-plugin-sass",
+        "gatsby-plugin-image",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+    ],
 };
